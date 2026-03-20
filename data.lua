4a70fd9e4c8977c27ba1cfdbe258f51157991449
UntouchedWagons <untouchedwagons@fastmail.com>
2026-03-19 21:05:57 -0400

    Initial release

---
+++ data.lua
@@ -0,0 +1,57 @@
+--data.lua
+
+data.raw.plant["yumako-tree"].autoplace.probability_expression = "0.005";
+data.raw.plant["yumako-tree"].autoplace.tile_restriction =
+{
+  "natural-yumako-soil", 
+  "artificial-yumako-soil", 
+  "overgrowth-yumako-soil",
+  "midland-cracked-lichen",
+  "midland-cracked-lichen-dark",
+  "midland-cracked-lichen-dull",
+  "midland-turquoise-bark",
+  "midland-turquoise-bark-2",
+  "highland-dark-rock",
+  "highland-dark-rock-2",
+  "lowland-cream-cauliflower",
+  "lowland-cream-cauliflower-2",
+  "lowland-dead-skin",
+  "lowland-dead-skin-2",
+  "highland-yellow-rock",
+  "midland-yellow-crust",
+  "midland-yellow-crust-2",
+  "midland-yellow-crust-3",
+  "midland-yellow-crust-4",
+  "lowland-red-vein",
+  "lowland-red-vein-2",
+  "lowland-red-vein-3",
+  "lowland-red-vein-4"
+};
+
+data.raw.plant["jellystem"].autoplace.probability_expression = "0.005";
+data.raw.plant["jellystem"].autoplace.tile_restriction =
+{
+  "natural-yumako-soil", 
+  "artificial-yumako-soil", 
+  "overgrowth-yumako-soil",
+  "midland-cracked-lichen",
+  "midland-cracked-lichen-dark",
+  "midland-cracked-lichen-dull",
+  "midland-turquoise-bark",
+  "midland-turquoise-bark-2",
+  "highland-dark-rock",
+  "highland-dark-rock-2",
+  "lowland-cream-cauliflower",
+  "lowland-cream-cauliflower-2",
+  "lowland-dead-skin",
+  "lowland-dead-skin-2",
+  "highland-yellow-rock",
+  "midland-yellow-crust",
+  "midland-yellow-crust-2",
+  "midland-yellow-crust-3",
+  "midland-yellow-crust-4",
+  "lowland-red-vein",
+  "lowland-red-vein-2",
+  "lowland-red-vein-3",
+  "lowland-red-vein-4"
+};
